be72babae1da5dceb3964abb9a5187b70ec43fb6
wwwAJun <73609544+wwwAJua@users.noreply.github.com>
2020-12-21 14:16:45 +0200

    #12 Показ модальных окон на Jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function () {
-
+	/*Filter
+	---------------------------*/
 	let filter = $("[data-filter]");
 
 	filter.on("click", function (event) {
@@ -20,5 +21,59 @@ $(function () {
 				}
 			});
 		}
+	});
+
+	/*Modals
+	---------------------------*/
+	const modalCall = $('[data-modal]');
+	const modalClose = $('[data-close]');
+
+	modalCall.on('click', function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalId = $this.data('modal');
+
+		$(modalId).addClass('show');
+		$('body').addClass('no-scroll');
+
+		setTimeout(function () {
+			$(modalId).find('.modal_dialog').css({
+				transform: 'rotateX(0)'/*'scale(1)'*/
+			});
+		}, 200);
+	});
+
+	modalClose.on('click', function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalParent = $this.parents('.modal');
+
+		modalParent.find('.modal_dialog').css({
+			transform: 'rotateX(90deg)'/*'scale(0)'*/
+		});
+
+		setTimeout(function () {
+			modalParent.removeClass('show');
+			$('body').removeClass('no-scroll');
+		}, 200);
+
+	});
+
+	$('.modal').on('click', function (event) {
+		let $this = $(this);
+		$this.find('.modal_dialog').css({
+			transform: 'rotateX(90deg)'/*'scale(0)'*/
+		});
+
+		setTimeout(function () {
+			$this.removeClass('show');
+			$('body').removeClass('no-scroll');
+		}, 200);
+	});
+
+	$('.modal_dialog').on('click', function (event) {
+		event.stopPropagation();
 	});
 });
